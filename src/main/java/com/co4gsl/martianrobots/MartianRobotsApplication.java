f3c5c2473efcebfd96505a24e5f63c81f6f3420c
RPAYAL <rpayal@hotmail.com>
2020-11-07 19:32:01 +0000

    Initial application commit with Spring Boot

---
+++ src/main/java/com/co4gsl/martianrobots/MartianRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.co4gsl.martianrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianRobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartianRobotsApplication.class, args);
+	}
+
+}
